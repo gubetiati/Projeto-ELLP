ba4f3a6dcac5ac6ac5492192ea5d1e685a69e0d6
SamuelPjacobsen <samuel.jacobsen@startse.com>
2025-02-07 22:33:30 -0300

    refactor: update auth to include admin collaborator

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,6 +9,13 @@ export const login = async (credentials) => {
 };
 
 export const registerColaborador = async (colaboradorData) => {
-    const response = await api.post('/register/colaborador', colaboradorData);
-    return response.data;
+  return api.post('/register/colaborador', {
+    ...colaboradorData,
+    role: "ADMIN"
+  });
+};
+
+export const registerUser = async (userData) => {
+    return api.post('/register/user', userData);
   };
+  
