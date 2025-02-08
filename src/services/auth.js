b04159d20cf9ed88f81b0f40ee3b00b307124965
SamuelPjacobsen <samuel.jacobsen@startse.com>
2025-02-07 22:26:42 -0300

    feat: add auth file to handle token

---
+++ src/services/auth.js
@@ -0,0 +1,14 @@
+import api from './api';
+
+export const login = async (credentials) => {
+  const response = await api.post('/login', credentials);
+  if (response.data.token) {
+    localStorage.setItem('token', response.data.token);
+  }
+  return response.data;
+};
+
+export const registerColaborador = async (colaboradorData) => {
+    const response = await api.post('/register/colaborador', colaboradorData);
+    return response.data;
+  };
